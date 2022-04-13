bb5400adea41254e3649313bf83538cd85c983a0
HaniOlwan <heenoow@gmail.com>
2022-04-13 15:48:13 +0300

    created artists card Relates #6

--- src/App.jsx
+++ src/App.jsx
@@ -62,6 +62,36 @@ function App() {
           </button>
         </li>
       </ul>
+      <div className="artists">
+        <div className="card">
+          <img src="https://i.scdn.co/image/ab676161000051749d3f9efc8e0030306e583cef" alt="Error" />
+          <p>Justin Temperlake</p>
+          <p>Justin Temperlake</p>
+          <small>classic</small>
+          <small>pop</small>
+        </div>
+        <div className="card">
+          <img src="https://i.scdn.co/image/ab676161000051749d3f9efc8e0030306e583cef" alt="Error" />
+          <p>Justin Temperlake</p>
+          <p>Justin Temperlake</p>
+          <small>classic</small>
+          <small>pop</small>
+        </div>
+        <div className="card">
+          <img src="https://i.scdn.co/image/ab676161000051748ae7f2aaa9817a704a87ea36" alt="Error" />
+          <p>Justin Temperlake</p>
+          <p>Justin Temperlake</p>
+          <small>classic</small>
+          <small>pop</small>
+        </div>
+        <div className="card">
+          <img src="https://i.scdn.co/image/ab676161000051748ae7f2aaa9817a704a87ea36" alt="Error" />
+          <p>Justin Temperlake</p>
+          <p>Justin Temperlake</p>
+          <small>classic</small>
+          <small>pop</small>
+        </div>
+      </div>
     </div>
   );
 }
